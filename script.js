af14f2fec43993d61cd6fbb87039c817cc8e4efa
RaninduAsus <157501577+raninduwathsal@users.noreply.github.com>
2024-03-13 16:22:17 +0530

    Made teh code cleaner implemented game logic finished project

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let counter;
 let computerSelection;
+let playerSelection;
 //initalize score counter for player and computer
 let playerScore = 0;
 let computerScore = 0;
@@ -12,30 +13,28 @@ let message;
 // ask computer for a choice rock, paper or scissors
 
 function getComputerChoice() {
-    let choice = Math.floor((Math.random() * 3) + 1);
-    if (choice === 1) {
-      return "rock";
-     } else if (choice === 2) {
-      return  "paper"; 
-     } else (choice === 3) 
-       return "scissors";
-    }
+    let choice = ["rock", "paper", "scissors"];
+  
+    const items = choice[Math.floor(Math.random() * choice.length)];
+    return items;
+  }
+
 // get the player input convert it to lowercase
 
-function playerSelection() {
+function playerSel() {
     let choice = prompt(" please enter rock, paper or scissors");
     playChoice = choice.toLowerCase();
+    return playChoice;
 }
 
 function playFive() {
     
     for (counter = 0; counter <= 5; counter++){
       if (counter < 5) {
-        playerSelection();  
-        getComputerChoice();
-        playGame();
-        
-        score();
+        playerSelection = playerSel();  
+        computerSelection = getComputerChoice();
+        console.log(logic(playerSelection, computerSelection));
+
       } else if (counter = 5) {
         
         if (computerScore < playerScore) {
@@ -48,4 +47,39 @@ function playFive() {
   
     } 
   } 
+  function logic(playerSelection, computerSelection) {
+    // your code here!
+    if (playerSelection === computerSelection) {
+      return "Its a draw 🤝";
+    }
   
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+      computerScore++;
+      return "You lose mate 🤡";
+    }
+  
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+      playerScore++;
+      return "You win 🏆";
+    }
+  
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+      playerScore++;
+      return "You win 🏆";
+    }
+  
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+      playerScore++;
+      return "You win 🏆";
+    }
+  
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+      computerScore++;
+      return "You lose mate 🤡";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+      computerScore++;
+      return "You lose mate 🤡";
+    }
+  }
+  playFive()
