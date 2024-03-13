add705af4fae9be0c8c75d41b08a7c5ff04393d7
RaninduAsus <157501577+raninduwathsal@users.noreply.github.com>
2024-03-13 16:04:36 +0530

    get pc choice and user choice implement

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//initalize two variables for the playerChoice and computerChoice
+// Global Variables for keeping track of score 
 
 let counter;
 let computerSelection;
@@ -9,7 +9,20 @@ let computerScore = 0;
 let result;
 //set variable to allow result of playGame function to be used to calculate score
 let message;
-// ask player for a choice rock, paper or scissors
+// ask computer for a choice rock, paper or scissors
 
+function getComputerChoice() {
+    let choice = Math.floor((Math.random() * 3) + 1);
+    if (choice === 1) {
+      return "rock";
+     } else if (choice === 2) {
+      return  "paper"; 
+     } else (choice === 3) 
+       return "scissors";
+    }
+// get the player input convert it to lowercase
 
-  
+function playerSelection() {
+    let choice = prompt(" please enter rock, paper or scissors");
+    playChoice = choice.toLowerCase();
+}
